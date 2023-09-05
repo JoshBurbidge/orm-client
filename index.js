0796be2f373f8a8bb251f1eb5c63d820f327786a
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2023-09-04 19:53:25 -0500

    update to use connection class API

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import { Uuid, connect, disconnect, raw } from 'orm';
+import { Uuid, connect } from 'orm';
 
 console.log(new Uuid().v4())
 
 async function run() {
   const connection = await connect();
-  const result = await raw(connection, 'select * from user')
+  const result = await connection.raw('select * from user')
   console.log(result)
 
-  await disconnect(connection);
+  await connection.disconnect();
 }
 
 run()
