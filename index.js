5b0215448a766b056d6d099ea2700bd69f33e71a
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2023-09-04 19:30:57 -0500

    use raw query function

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-import { Uuid, connect, disconnect, uuidObject } from 'orm';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { Uuid, connect, disconnect, raw } from 'orm';
 
 console.log(new Uuid().v4())
-console.log(uuidObject.v4())
 
 async function run() {
   const connection = await connect();
+  const result = await raw(connection, 'select * from user')
+  console.log(result)
+
   await disconnect(connection);
 }
+
 run()
