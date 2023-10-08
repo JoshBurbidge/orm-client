7aa9a69ab06e8f0fe5448fa2fdf028172c9931bd
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2023-10-08 13:19:27 -0500

    use object as constructor parameter

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import { connect, init } from 'orm';
+import { init } from 'orm';
 import { User } from './model/User.js';
 
 async function run() {
   const connection = await init()
 
-
   const result = await User.findAll();
   console.log(result)
 
   const one = await User.findById(24);
-  console.log(one)
 
-  const newUser = await new User('newUser5', 'newName2', 'customer').save();
+  const newUser = await new User({
+    username: 'newUser8',
+    name: 'newName2',
+    type: 'customer'
+  }).save();
   console.log('new: ', newUser)
 
   await connection.disconnect();
--- model/User.js
+++ model/User.js
@@ -2,10 +2,10 @@ import { Table } from "orm";
 
 export class User extends Table {
 
-  constructor(username, name, type) {
+  constructor(userOptions) {
     super();
-    this.username = username;
-    this.name = name;
-    this.type = type;
+    this.username = userOptions.username;
+    this.name = userOptions.name;
+    this.type = userOptions.type;
   }
 }
